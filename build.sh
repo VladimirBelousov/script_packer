6352affb5fb3de55decb8eb6129e08b900581d9e
Belousov Vladimir V <vl.bel@tut.by>
2021-09-07 10:48:36 +0300

    Some text clarification

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright 2013-2014, 2021 Vladimir Belousov (vlad.belos@gmail.com)
-# https://github.com/VladimirBelousov/fancy_scripts
+# https://github.com/VladimirBelousov/script_packer
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
